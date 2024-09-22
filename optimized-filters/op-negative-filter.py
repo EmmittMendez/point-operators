311eca30c23b9eaeeea38fb522430c0e9e4e892f
EmmittMendez <77180330+EmmittMendez@users.noreply.github.com>
2024-09-22 17:53:01 -0600

    Add negative filter

---
+++ optimized-filters/op-negative-filter.py
@@ -0,0 +1,50 @@
+import argparse
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--image", required=True, help="Path to the input image")
+args = vars(parser.parse_args())
+
+image_BGR = cv2.imread(args["image"])
+image_RGB = cv2.cvtColor(image_BGR, cv2.COLOR_BGR2RGB)
+
+# Calculamos el mapeo del filtro negativo y lo almacenamos en el arreglo "y2"
+max_val = image_RGB.max()   # Obtenemos el valor máximo de los 3 canales de la imagen
+
+# Construimos un vector X y Y para almacenar los valores
+# del filtro negativo . El vector Y servira para la transformación de 
+# los valores de la imagen, usando programación dinamica
+x = np.linspace(0, 255,256)
+y2 = np.array([max_val - i for i in range(256)], dtype='uint8')
+
+# Definimos una función lambda para el filtro negativo
+negative = lambda m: y2[m]
+
+# Se aplica la función lambda a cada pixel de la imagen. Se vectoriza la
+# matriz para que pueda ser procesado por la función lambda
+image_RGB_negative = np.array(np.vectorize(negative)(image_RGB), dtype='uint8')
+
+
+
+fig = plt.figure(figsize=(9,6))
+ax1 = fig.add_subplot(2,2,1)
+ax2 = fig.add_subplot(2,2,2)
+ax3 = fig.add_subplot(2,3,5)
+
+ax1.imshow(image_RGB)
+ax1.set_title("Original Image")
+
+ax2.imshow(image_RGB_negative)
+ax2.set_title("Negative Image")
+
+x = np.linspace(0, 255, 255)
+y1 = x
+y2 = np.array(255 - x)
+ax3.plot(x, y1, color="r", linewidth=1, label = "Id. Func.")
+msg = "Negative Func."
+ax3.plot(x, y2, color="b", linewidth=1, label = msg)
+ax3.legend()
+ax3.set_title("Negative Function")
+plt.show()
